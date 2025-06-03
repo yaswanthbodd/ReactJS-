e68550c068922b45f01d78a2806d1a868a3b4a7c
yaswanthbodd <bodduyaswanth09@gmail.com>
2025-06-03 17:00:23 +0530

    Dynamic Form Creation for register Page

--- ReactJs/src/components/config/index.js
+++ ReactJs/src/components/config/index.js
@@ -16,3 +16,30 @@ export const loginFormElements = [
         type: 'password'
     }
 ]
+
+export const RegisterFormElements = [
+    {
+        name : 'name',
+        id : 'name',
+        placeholder : 'Enter your name..',
+        label : 'Name',
+        componentType : 'input',
+        type : 'text'
+    },
+    {
+        name : 'email',
+        id : 'email',
+        placeholder : 'Enter your email...',
+        label : 'Email',
+        componentType : 'input',
+        type : 'email'
+    },
+    {
+        name : 'password',
+        id : 'password',
+        placeholder : 'Enter your password',
+        label : 'Password',
+        componentType : 'input',
+        type : 'password'
+    }
+]
--- ReactJs/src/components/register/index.jsx
+++ ReactJs/src/components/register/index.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import CommonForm from "../common-form";
+import { RegisterFormElements } from "../config";
+
+const initialRegisterFormData = {
+    name : '',
+    email : '',
+    password : ''
+}
+
 function RegisterComponent() {
+    const [registerFormData , setRegisterFormData] = useState(initialRegisterFormData);
+
+    function handleRegisterOnSubmit(event){
+        event.preventDefault();
+        console.log(registerFormData);
+
+        //api logic and database logic
+
+        //Default Values on submit the form
+        setRegisterFormData(initialRegisterFormData);
+    }
     return(
         <div>
+
             <h1>Register Page/Component</h1>
+            <CommonForm 
+                formControls={RegisterFormElements}
+                formData = {registerFormData}
+                setFormData = {setRegisterFormData}
+                buttonText={'Register'}
+                onHandleSubmit={handleRegisterOnSubmit}
+            />
         </div>
     )
 }
