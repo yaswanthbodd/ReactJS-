72df9bb35e74191ff2102bdce07b9e2dc1a03cf9
yaswanthbodd <bodduyaswanth09@gmail.com>
2025-06-04 18:02:01 +0530

    useWindowResize Hook

--- react-router-dom-Example/src/pages/recipes/index.jsx
+++ react-router-dom-Example/src/pages/recipes/index.jsx
@@ -1,9 +1,11 @@
 import { useLocation } from "react-router-dom";
 import useFetch from "../../hooks/use-fetch";
+import useWindowSize from "../../hooks/use-window-resize";
 
 function RecipeList() {
     const location = useLocation();
     //console.log(location);
+    const windowSize = useWindowSize();
 
     //Using Custom Hooks to Fetch the Data
     const {data, loading, error} = useFetch('https://dummyjson.com/recipes');
@@ -14,7 +16,7 @@ function RecipeList() {
     }
     return(
         <div>
-            <h1>Recipe Item Page</h1>
+            <h1 style={{ color : windowSize.width < 768 ? 'red' : 'blue' }}>Recipe Item Page</h1>
             <ul>
                 {
                     data?.recipes?.length > 0 ?
